a1c0bbba8301ca7b9a23ad624adf39d96201a95b
samarth goyal <samarthagrawal1804@gmail.com>
2023-09-11 23:38:28 +0530

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
    <>
-   <h1 className="text-5xl bg-slate-700 text-white text-center">Hello developers...</h1>
+   <h1 className="text-xl bg-slate-700 text-white text-center">Hello developers...</h1>
    </>
   )
 }
